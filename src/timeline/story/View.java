7362b534950e5d3361725346c525d74bf9ae2732
ChrisQuinto <ch.paffrath@gmx.net>
2017-08-21 16:42:50 +0200

    drawString title + Ellipses

--- src/timeline/story/View.java
+++ src/timeline/story/View.java
@@ -2,6 +2,7 @@ package timeline.story;
 
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.geom.Ellipse2D;
 import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
 import java.util.ArrayList;
@@ -15,7 +16,8 @@ import timeline.story.Data;
 
 public class View extends JPanel {
 	ArrayList<Point2D> storyPoints = new ArrayList<Point2D>();
-
+	ArrayList<Ellipse2D> storyEllipses = new ArrayList<Ellipse2D>();
+	
 	public void paint(Graphics g) {
 		Graphics2D g2D = (Graphics2D)g;
         int midX = getWidth()/2;
@@ -40,21 +42,30 @@ public class View extends JPanel {
 //        System.out.println(sorted_events);
         
         double storyLength = start.distance(end);
-        double freeSpace = storyLength*0.01;
-        storyLength = storyLength*0.98;
+        double freeSpace = storyLength*0.02;
+        storyLength = storyLength*0.92;
         double storyStep = storyLength/range;
         int currentSegment = firstInsertedEntry.getKey();
         
         for(int i = 0; i <= range; i++) {
-        	Point2D currentStoryPoint = new Point2D.Double(midX-3,start.getY()-(freeSpace+i*storyStep));
+        	Point2D currentStoryPoint = new Point2D.Double(midX,start.getY()-(freeSpace+i*storyStep));
         	if (sorted_events.get(currentSegment) != null) {
+        		Ellipse2D storyEllipse = new Ellipse2D.Double(currentStoryPoint.getX()-2.5, currentStoryPoint.getY()-2.5,5.0,5.0);
+     
         		ArrayList<String> label = sorted_events.get(currentSegment);
-        		g2D.drawOval((int)currentStoryPoint.getX(), (int)currentStoryPoint.getY(), 6, 1);
+        		String titel = label.get(0);
+        		String date = label.get(1);
+        		String content = label.get(2);
+        		String characters = label.get(3);
+        		
+        		g2D.draw(storyEllipse);
+                g2D.drawString(titel, (int)currentStoryPoint.getX() + 5, (int)currentStoryPoint.getY() - 1);
         		currentSegment += 1;
         		storyPoints.add(currentStoryPoint);
-        			} else {
+        		storyEllipses.add(storyEllipse);
+        	} else {
         		currentSegment += 1;
-        		}
+        	}
         }
         System.out.println(storyPoints);
 	}
