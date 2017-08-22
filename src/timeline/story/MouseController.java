174a61ac5bdf94e5e828d7078e9a922abb900b95
Xeern <re1307@web.de>
2017-08-22 17:08:54 +0200

    more mouse interaction, added new Map

--- src/timeline/story/MouseController.java
+++ src/timeline/story/MouseController.java
@@ -1,17 +1,34 @@
 package timeline.story;
 
+import java.awt.Color;
+import java.awt.Graphics2D;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Point2D;
+import java.util.ArrayList;
+
+import timeline.story.View;
 
 public class MouseController implements MouseListener,MouseMotionListener {
 	
 	private View view = null;
 	
 	public void mouseClicked(MouseEvent e) {
-		
+		int x = e.getX();
+		int y = e.getY();
+		for(int i = 0; i < view.storyEllipses.size(); ++i) {
+			Ellipse2D tmp = view.storyEllipses.get(i);
+			if(tmp.contains(x, y)) {
+				Point2D currentPoint = view.storyPoints.get(i);
+				int currentSegment = view.segmentAtPoint.get(currentPoint);
+				ArrayList<String> currentEvent = view.sorted_events.get(currentSegment);
+				System.out.println(currentEvent);
+			} else {
+
+			}
+		}
 	}
 	
 	public void mouseEntered(MouseEvent arg0) {
@@ -37,10 +54,17 @@ public class MouseController implements MouseListener,MouseMotionListener {
 	public void mouseMoved(MouseEvent e){
 		int x = e.getX();
 		int y = e.getY();
+		Graphics2D currentGraphic = (Graphics2D)view.getGraphics();
 		for(int i = 0; i < view.storyEllipses.size(); ++i) {
 			Ellipse2D tmp = view.storyEllipses.get(i);
 			if(tmp.contains(x, y)) {
-				System.out.println((tmp.getCenterY()));
+				currentGraphic.setColor(Color.BLUE);
+				currentGraphic.draw(tmp);
+				currentGraphic.fill(tmp);
+			} else {
+				currentGraphic.setColor(Color.BLACK);
+				currentGraphic.draw(tmp);
+				currentGraphic.fill(tmp);
 			}
 		}
 	}
--- src/timeline/story/View.java
+++ src/timeline/story/View.java
@@ -1,11 +1,13 @@
 package timeline.story;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -17,6 +19,9 @@ import timeline.story.Data;
 public class View extends JPanel {
 	ArrayList<Point2D> storyPoints = new ArrayList<Point2D>();
 	ArrayList<Ellipse2D> storyEllipses = new ArrayList<Ellipse2D>();
+	Map<Integer, ArrayList<String>> sorted_events = new TreeMap<Integer, ArrayList<String>>(Data.events);
+	Map<Point2D,Integer> segmentAtPoint = new HashMap<Point2D,Integer>();
+	Color pointColor = Color.BLACK;
 	
 	public void paint(Graphics g) {
 		Graphics2D g2D = (Graphics2D)g;
@@ -28,7 +33,7 @@ public class View extends JPanel {
         Point2D start = mainLine.getP2();
         Point2D end = mainLine.getP1();
         
-        Map<Integer, ArrayList<String>> sorted_events = new TreeMap<Integer, ArrayList<String>>(Data.events);
+//        Map<Integer, ArrayList<String>> sorted_events = new TreeMap<Integer, ArrayList<String>>(Data.events);
         int ecount = sorted_events.size();
         
         List<Entry<Integer, ArrayList<String>>> list = new ArrayList<Entry<Integer, ArrayList<String>>>(sorted_events.entrySet());
@@ -50,7 +55,7 @@ public class View extends JPanel {
         for(int i = 0; i <= range; i++) {
         	Point2D currentStoryPoint = new Point2D.Double(midX,start.getY()-(freeSpace+i*storyStep));
         	if (sorted_events.get(currentSegment) != null) {
-        		Ellipse2D storyEllipse = new Ellipse2D.Double(currentStoryPoint.getX()-2.5, currentStoryPoint.getY()-2.5,5.0,5.0);
+        		Ellipse2D storyEllipse = new Ellipse2D.Double((int)currentStoryPoint.getX()-2.5, (int)currentStoryPoint.getY()-2.5,5.0,5.0);
      
         		ArrayList<String> label = sorted_events.get(currentSegment);
         		String titel = label.get(0);
@@ -58,11 +63,14 @@ public class View extends JPanel {
         		String content = label.get(2);
         		String characters = label.get(3);
         		
+                g2D.setColor(pointColor);
         		g2D.draw(storyEllipse);
+        		g2D.fill(storyEllipse);
                 g2D.drawString(titel, (int)currentStoryPoint.getX() + 5, (int)currentStoryPoint.getY() - 1);
-        		currentSegment += 1;
+                segmentAtPoint.put(currentStoryPoint, currentSegment);
         		storyPoints.add(currentStoryPoint);
         		storyEllipses.add(storyEllipse);
+        		currentSegment += 1;
         	} else {
         		currentSegment += 1;
         	}
