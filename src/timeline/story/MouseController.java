5425b2aa88abc33597a45e1c6975a0e3a5b455a8
Xeern <re1307@web.de>
2017-08-25 16:20:07 +0200

    points in detail view are now clickable

--- src/timeline/story/MouseController.java
+++ src/timeline/story/MouseController.java
@@ -19,14 +19,25 @@ public class MouseController implements MouseListener,MouseMotionListener {
 		int x = e.getX();
 		int y = e.getY();
 		for(int i = 0; i < view.storyEllipses.size(); ++i) {
-			Ellipse2D tmp = view.storyEllipses.get(i);
-			if(tmp.contains(x, y)) {
-				Point2D currentPoint = view.storyPoints.get(i);
-				int currentSegment = view.segmentAtPoint.get(currentPoint);
-				ArrayList<String> currentEvent = view.sorted_events.get(currentSegment);
-				System.out.println(currentEvent);
-			} else {
-
+			if(view.overview == true) {
+				Ellipse2D tmp = view.storyEllipses.get(i);
+				if(tmp.contains(x, y)) {
+					Point2D currentPoint = view.storyPoints.get(i);
+					int currentSegment = view.segmentAtPoint.get(currentPoint);
+					ArrayList<String> currentEvent = view.sorted_events.get(currentSegment);
+					System.out.println(currentEvent);
+				}
+			}
+			if(view.detailView == true) {
+				double tmpX = view.storyEllipses.get(i).getMinX()-3;
+				double tmpY = (view.storyEllipses.get(i).getMinY()-view.getHeight()*2/4)*2;
+				Ellipse2D tmp = new Ellipse2D.Double(tmpX, tmpY, 10, 10);
+				if(tmp.contains(x,y)) {
+					Point2D currentPoint = view.storyPoints.get(i);
+					int currentSegment = view.segmentAtPoint.get(currentPoint);
+					ArrayList<String> currentEvent = view.sorted_events.get(currentSegment);
+					System.out.println(currentEvent);
+				}
 			}
 		}
 	}
@@ -55,14 +66,28 @@ public class MouseController implements MouseListener,MouseMotionListener {
 		int x = e.getX();
 		int y = e.getY();
 		Graphics2D currentGraphic = (Graphics2D)view.getGraphics();
-		if(view.overview == true) {
-			for(int i = 0; i < view.storyEllipses.size(); ++i) {
+		for(int i = 0; i < view.storyEllipses.size(); ++i) {
+			if(view.overview == true) {
 				Ellipse2D tmp = view.storyEllipses.get(i);
 				if(tmp.contains(x, y)) {
 					currentGraphic.setColor(Color.BLUE);
 					currentGraphic.draw(tmp);
 					currentGraphic.fill(tmp);
 				} else {
+					currentGraphic.setColor(Color.BLACK);
+					currentGraphic.draw(tmp);
+					currentGraphic.fill(tmp);
+				}
+			}
+			if(view.detailView == true) {
+				double tmpX = view.storyEllipses.get(i).getMinX()-3;
+				double tmpY = (view.storyEllipses.get(i).getMinY()-view.getHeight()*2/4)*2;
+				Ellipse2D tmp = new Ellipse2D.Double(tmpX, tmpY, 10, 10);
+				if(tmp.contains(x,y)) {
+					currentGraphic.setColor(Color.BLUE);
+					currentGraphic.draw(tmp);
+					currentGraphic.fill(tmp);
+ 				} else {
 					currentGraphic.setColor(Color.BLACK);
 					currentGraphic.draw(tmp);
 					currentGraphic.fill(tmp);
