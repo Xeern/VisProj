46aaaf5629c48ed0f89710ed969f662cabcfcad9
ChrisQuinto <ch.paffrath@gmx.net>
2017-08-25 17:52:54 +0200

    add date + vertical labels (not working yet)

--- src/timeline/story/View.java
+++ src/timeline/story/View.java
@@ -1,8 +1,11 @@
 package timeline.story;
 
 import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.LayoutManager;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
@@ -121,6 +124,9 @@ public class View extends JPanel {
         storyLength = storyLength*0.92;
         double storyStep = storyLength/range;
         int currentSegment = firstInsertedEntry.getKey();
+        
+        int datecounter = 0; // to check if there are duplicate dates
+        Point2D DcheckPoint = mainLine.getP2(); // date check point 
 		
 		storyPoints.clear();
 		storyEllipses.clear();
@@ -129,17 +135,64 @@ public class View extends JPanel {
         	if (sorted_events.get(currentSegment) != null) {
         		Ellipse2D storyEllipse = new Ellipse2D.Double((int)currentStoryPoint.getX()-2.5, (int)currentStoryPoint.getY()-2.5,5.0,5.0);
      
-        		JPanel temp = new JPanel();
-        		temp.setBounds((int)currentStoryPoint.getX() + 5, (int)currentStoryPoint.getY() - 21, 200, 20);
-        		ArrayList<String> label = sorted_events.get(currentSegment);
-        		String title = label.get(0);
+        		ArrayList<String> labels = sorted_events.get(currentSegment);
+        		String title = labels.get(0);
+        		
+        		JPanel viewPanel = new JPanel();
+        		viewPanel.setLayout((LayoutManager) new FlowLayout(FlowLayout.LEFT));
+        		viewPanel.setBounds((int)currentStoryPoint.getX() + 5, (int)currentStoryPoint.getY() - 21, 200, 20);
         		JLabel jtitle = new JLabel(title);
-        		String date = label.get(1);
-        		String content = label.get(2);
-        		String characters = label.get(3);
-        		temp.add(jtitle);
-        		this.add(temp);
-        		temp.updateUI();
+        		viewPanel.add(jtitle);
+        		this.add(viewPanel);
+        		viewPanel.updateUI();
+        		 
+        		String date = labels.get(1);
+        		
+        		for(int j = 1; j<=31; j++) { // check for the next month if the date occurs more than once
+        			if(sorted_events.get(currentSegment+j) != null) {                		
+        				ArrayList<String> testlabels = sorted_events.get(currentSegment+j);
+        				String testdate = testlabels.get(1);
+        				System.out.println(date + testdate); // !ERROR! last print = "September 2017September 2017", so why no increased counter???
+        				if (date == testdate) { // if so raise the counter
+        					datecounter += 1;
+        					DcheckPoint = new Point2D.Double(midX,start.getY()-(freeSpace+i*storyStep));
+        					System.out.println("found duplicate");
+        				}
+        			}
+        		}
+        		
+        		System.out.println(datecounter);
+        		
+        		if (datecounter == 1) { // that should mean that you can draw the label vertical from first duplicate to last
+        			int counterstart = i-datecounter; // get range
+        			DcheckPoint = new Point2D.Double(midX,start.getY()-(freeSpace+counterstart*storyStep));
+        			int yrange = (int)currentStoryPoint.getY() - (int)DcheckPoint.getY();
+        			JPanel datePanel = new JPanel();
+            		datePanel.setLayout((LayoutManager) new FlowLayout(FlowLayout.RIGHT));
+            		datePanel.setBounds((int)currentStoryPoint.getX() - 105, (int)currentStoryPoint.getY() -18, 100, yrange);
+            		JLabel jdate = new JLabel(date);
+        			jdate.setFont(new Font("Courier New",Font.ITALIC,8));
+        			jdate.setText(verticalText(date));
+        			datePanel.add(jdate);
+        			this.add(datePanel);
+        			datePanel.updateUI();
+        			datecounter = 0;
+        		} else if (datecounter > 1) {
+        			datecounter = 1;
+        		} else {
+        			JPanel datePanel = new JPanel();
+            		datePanel.setLayout((LayoutManager) new FlowLayout(FlowLayout.RIGHT));
+            		datePanel.setBounds((int)currentStoryPoint.getX() - 105, (int)currentStoryPoint.getY() -18, 100, 20);
+        			JLabel jdate = new JLabel(date);
+        			jdate.setFont(new Font("Courier New",Font.ITALIC,10));
+        			datePanel.add(jdate);
+        			this.add(datePanel);
+        			datePanel.updateUI();
+        		}
+        		
+//        		String content = labels.get(2);
+//        		String characters = labels.get(3);
+        		
         		
         		g2D.draw(storyEllipse);
         		g2D.fill(storyEllipse);
@@ -152,5 +205,16 @@ public class View extends JPanel {
         		currentSegment += 1;
         	}
         }
+	}
+
+	public static String verticalText(String horizontal) { // fancy vertical label
+		String temp = "<html>";
+		String newline = "<br>";
+		String [] splitter = horizontal.split("");
+		for (String letter : splitter) {
+			temp += letter + newline;
+		}
+		temp += "</html>";
+		return temp;
 	}
 }
